d4748729fa0a66f23928886b63d7bb6587e2cfe5
mikail53 <mikaildoank5@gmail.com>
2025-08-20 21:18:12 +0700

    jangar

--- laravel_WargaKita/app/Http/Controllers/Rw/DashboardController.php
+++ laravel_WargaKita/app/Http/Controllers/Rw/DashboardController.php
@@ -28,9 +28,14 @@ class DashboardController extends Controller
 
         $pengumuman_rt = Pengumuman::where('id_rt',$id_rt)->count();
         $jumlah_rt = Rukun_tetangga::count();
-        $jumlah_warga_penduduk = Warga::where('jenis', 'penduduk')->count();
-        $jumlah_warga_pendatang = Warga::where('jenis', 'pendatang')->count();
+        $total_pemasukan = Tagihan::where('status_bayar', 'sudah_bayar')->sum('nominal');
+        $total_pengeluaran = Transaksi::sum('pengeluaran');
+        $total_saldo_akhir = $total_pemasukan - $total_pengeluaran;
         $title = 'Dashboard';
-        return view('rw.dashboard.dashboard', compact('title','jumlah_warga','jumlah_kk','jumlah_pengumuman','jumlah_warga_penduduk','jumlah_warga_pendatang','jumlah_rt'));
+        return view('rw.dashboard.dashboard', compact('title','jumlah_warga','jumlah_kk','jumlah_pengumuman','jumlah_warga_penduduk','jumlah_warga_pendatang','jumlah_rt','total_pemasukan','total_pengeluaran', 'total_saldo_akhir'));
+        $jumlah_warga_penduduk = Warga::where('status_warga', 'penduduk')->count();
+        $jumlah_warga_pendatang = Warga::where('status_warga', 'pendatang')->count();
+        $title = 'Dashboard';
+        return view('rw.dashboard.dashboard', compact('title','jumlah_warga','jumlah_kk','pengumuman_rw','pengumuman_rt','jumlah_warga_penduduk','jumlah_warga_pendatang','jumlah_rt'));
     }
 }
